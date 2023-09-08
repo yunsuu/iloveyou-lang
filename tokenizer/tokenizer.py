5ba231233afb93f0aec8197eaf7b473e8818a4ad
yunsuu <ljsql934@gmail.com>
2023-09-08 22:19:52 +0900

    organize tokenizer code

---
+++ tokenizer/tokenizer.py
@@ -0,0 +1,62 @@
+import method
+
+codeText = ''
+with open('../code.txt', 'r') as code_file:
+    codeText = code_file.read()
+    # for line in code_file:
+    #     print(line)
+
+
+# print(codeText)
+
+def tokenizer():
+    i = 0
+    tokenList = []
+    while i < len(codeText):
+        if codeText[i] == 'I':
+            endIdx = method.findEndIdxI(codeText, i)
+            tokenList.append(["KEYWORD", codeText[i:endIdx+1]])
+            i = endIdx
+        elif codeText[i] == 'L':
+            endIdx = method.findEndIdxLove(codeText, i)
+            tokenList.append(["KEYWORD", codeText[i:endIdx+1]])
+            i = endIdx
+        elif codeText[i] == 'Y':
+            endIdx = method.findEndIdxYou(codeText, i)
+            tokenList.append(["KEYWORD", codeText[i:endIdx+1]])
+            i = endIdx
+        elif codeText[i] == 'B':
+            endIdx = method.findEndIdxBecause(codeText, i)
+            tokenList.append(["KEYWORD", codeText[i:endIdx+1]])
+            i = endIdx
+        elif codeText[i] == 'D':
+            endIdx = method.findEndIdxDateWithMe(codeText, i)
+            tokenList.append(["KEYWORD", codeText[i:endIdx+1]])
+            i = endIdx
+        elif codeText[i] == 'H':
+            endIdx = method.findEndIdxHeyIHaveSomethingToTellYou(codeText, i)
+            tokenList.append(["KEYWORD", codeText[i:endIdx+1]])
+            i = endIdx
+        elif codeText[i] == 'W':
+            endIdx = method.findEndIdxWouldYouLikeYoBeMyGirlfriend(codeText, i)
+            tokenList.append(["KEYWORD", codeText[i:endIdx+1]])
+            i = endIdx
+        elif codeText[i] == '.' or codeText[i] == ',':
+            startIdx = i
+            while(True):
+                if codeText[i] == '.' or codeText[i] == ',':
+                    i+=1
+                else:
+                    break
+            endIdx = i-1
+            tokenList.append(["CONSTANT", codeText[startIdx:endIdx+1]])
+            i-=1
+        else:
+            if codeText[i] != '\n':
+                tokenList.append(["SYMBOL", codeText[i]])
+
+        i+=1
+    return tokenList
+        
+
+print(tokenizer())
